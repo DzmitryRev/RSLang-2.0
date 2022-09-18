e2f63c65166b15aade8d0a09b94079ecc0243729
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-09-18 21:10:25 +0300

    feat: create rendering logic for Header (two vars of Header: Minify and Default)

--- code/src/component/Header.tsx
+++ code/src/component/Header.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 
 type HeaderProps = {
   type: 'default' | 'minify'
 };
 
 function Header({ type }: HeaderProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
-    <div>
-      {type === 'default' ? 'Default Header' : 'Minify Header'}
-    </div>
+    <header className={cn('header', {
+      'header-default': type === 'default',
+      'header-minify': type === 'minify',
+    })}
+    >
+      <div className="logo-container">
+        {type}
+        Logo
+      </div>
+
+      <nav className={cn('navbar', {
+        'opened-navbar': isSidebarOpen,
+      })}
+      >
+        <ul>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li className="navbar-signInButton">
+            <button
+              type="button"
+            >
+              войти
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      <button type="button" className="signInButton">войти</button>
+      <button
+        type="button"
+        className="burger"
+        onClick={() => {
+          setIsSidebarOpen(!isSidebarOpen);
+          console.log(isSidebarOpen);
+        }}
+      >
+        burger
+      </button>
+    </header>
   );
 }
 
