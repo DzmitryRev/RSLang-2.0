0dc7e9bcb3dddb4e19127329f4e0f5405d96807a
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-09-18 21:12:18 +0300

    fix: rename component foulder to components

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import Header from './component/Header';
+import Header from './components/Header';
 
 function App() {
   const { pathname } = useLocation();
